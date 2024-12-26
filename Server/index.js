a2710df567a6e0054d90aa398d84f0965cb801c5
contributorsambhav <sambhav511974@gmail.com>
2024-12-26 20:25:17 +0530

    CORS url issue resolved

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://nullclassfirstproject.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
